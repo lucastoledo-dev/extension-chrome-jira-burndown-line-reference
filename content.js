ecce348c56fc248a749996ba41187eb1e03dced6
ltoledo <lucas.sg.rj@gmail.com>
2016-02-18 23:57:56 -0200

    Ajuste nos fatores dos calculos para melhor precisão nas horas.

--- content.js
+++ content.js
@@ -27,15 +27,49 @@ chrome.runtime.onMessage.addListener(
 			var h_fator2 = $($(".yAxis.y1Axis .tickLabel")[1]).text().replace(/[^0-9]/g, '');
 			var fator_reverse = $(".yAxis.y1Axis .tickLabel").last().text().replace(/[^0-9]/g, '');
 
-			var px_fator = (px_fator1-px_fator2);
-			var h_fator = (h_fator2-h_fator1);
+			//========================================Calc h_fator AND x_fator ========================================================
+			var ftxTemp = []
+			var fthTemp = []
+			var yAxisLength = $($(".yAxis.y1Axis .tickLabel")).length;
+			$($(".yAxis.y1Axis .tickLabel")).each(function(i,item){
+			 var xpoint_temp = $(item).attr("style").match("top:(.*)px;r")[1];
+			 var hpoint_temp = $(item).text().replace(/[^0-9]/g, '');
+
+			 if(i!=0 && i!=yAxisLength){
+			 	var xpoint_temp_before = $($(".yAxis.y1Axis .tickLabel")[i-1]).attr("style").match("top:(.*)px;r")[1];
+			 	var xresult = (xpoint_temp - xpoint_temp_before)*(-1);
+			 	ftxTemp.push(xresult); 
+
+			 	var hpoint_temp_before = $($(".yAxis.y1Axis .tickLabel")[i-1]).text().replace(/[^0-9]/g, '');
+			 	var hresult = (hpoint_temp - hpoint_temp_before);
+			 	fthTemp.push(hresult); 
+			 }
+			});
+			var XTotalSumyAxis = 0;
+			ftxTemp.forEach(function(item){ XTotalSumyAxis+=item });
+            var px_fator = XTotalSumyAxis/ftxTemp.length;
+			console.log("px_fator");
+			console.log(px_fator);
+
+			var HTotalSumyAxis = 0;
+			fthTemp.forEach(function(item){ HTotalSumyAxis+=item });
+			var h_fator = HTotalSumyAxis/fthTemp.length;
+			console.log("h_fator");
+			console.log(h_fator);
+			//==============================================================================================================
+
+
+
+
+			//var px_fator = (px_fator1-px_fator2);
+			//var h_fator = (h_fator2-h_fator1);
 			var hora_result = ((relY)*h_fator)/(px_fator);
 			var fator_alinhamento = 7;
 
-			var result = (((hora_result-fator_reverse+fator_alinhamento)*-1)).toFixed(2);
+			var result = (((hora_result-fator_reverse)*-1)).toFixed(2);
 
-			var lineReference = $('<span id="line_reference" style="border: 1px solid #00FF08;width: 98%;height: 0px;position: absolute;top: '+(relY+8.5)+'px;left: 27px;"></span>');
-			var valueHour = $('<span id="valueHour" style="top: '+(relY+8.5)+'px;z-index:9999;background-color: whitesmoke;color:red;width: 60px;height: 20px;position: absolute;left: -70px;padding: 4px;border-radius: 5px;text-align: center;border: 1px solid silver;"><b>'+result+'h</b></span>');
+			var lineReference = $('<span id="line_reference" style="border: 1px solid #00FF08;width: 98%;height: 0px;position: absolute;top: '+(relY)+'px;left: 27px;"></span>');
+			var valueHour = $('<span id="valueHour" style="top: '+(relY)+'px;z-index:9999;background-color: whitesmoke;color:red;width: 60px;height: 20px;position: absolute;left: -70px;padding: 4px;border-radius: 5px;text-align: center;border: 1px solid silver;"><b>'+result+'h</b></span>');
 
 			$current_line = {lineReference: lineReference,valueHour:valueHour};
 		   
@@ -45,8 +79,10 @@ chrome.runtime.onMessage.addListener(
 		    $("#ghx-chart-view").append($line_cache['lineReference']);
 			$("#ghx-chart-view").append($line_cache['valueHour']);
 		});
+
     }
   }
 
+  
 
 );
