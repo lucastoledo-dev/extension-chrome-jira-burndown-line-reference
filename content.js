f3abda6336f4805d48f5aa30cb1356918e21bfa3
ltoledo <lucas.sg.rj@gmail.com>
2016-02-19 00:16:40 -0200

    Ajuste nos fatores dos calculos para melhor precisão nas horas.

--- content.js
+++ content.js
@@ -18,7 +18,8 @@ chrome.runtime.onMessage.addListener(
 			var relX = e.pageX - parentOffset.left;
 			var relY = e.pageY - parentOffset.top;
 
-			$("#line_reference").remove("");
+			$("#line_h_reference").remove("");
+			$("#line_v_reference").remove("");
 			$("#valueHour").remove("");
 
 			var px_fator1 = $($(".yAxis.y1Axis .tickLabel")[0]).attr("style").match("top:(.*)px;r")[1];
@@ -45,6 +46,7 @@ chrome.runtime.onMessage.addListener(
 			 	fthTemp.push(hresult); 
 			 }
 			});
+
 			var XTotalSumyAxis = 0;
 			ftxTemp.forEach(function(item){ XTotalSumyAxis+=item });
             var px_fator = XTotalSumyAxis/ftxTemp.length;
@@ -58,22 +60,20 @@ chrome.runtime.onMessage.addListener(
 			console.log(h_fator);
 			//==============================================================================================================
 
-
-
-
-			//var px_fator = (px_fator1-px_fator2);
-			//var h_fator = (h_fator2-h_fator1);
 			var hora_result = ((relY)*h_fator)/(px_fator);
 			var fator_alinhamento = 7;
 
-			var result = (((hora_result-fator_reverse)*-1)).toFixed(2);
+			var result = (((hora_result-fator_reverse)*-1)+15).toFixed(2);
 
-			var lineReference = $('<span id="line_reference" style="border: 1px solid #00FF08;width: 98%;height: 0px;position: absolute;top: '+(relY)+'px;left: 27px;"></span>');
-			var valueHour = $('<span id="valueHour" style="top: '+(relY)+'px;z-index:9999;background-color: whitesmoke;color:red;width: 60px;height: 20px;position: absolute;left: -70px;padding: 4px;border-radius: 5px;text-align: center;border: 1px solid silver;"><b>'+result+'h</b></span>');
+			var lineHReference = $('<span id="line_h_reference" style="border: 1px solid #00FF08;width: 98%;height: 0px;position: absolute;top: '+(relY-23)+'px;left: 27px;"></span>');
+            var lineVReference = $('<span id="line_v_reference" style="border: 1px solid #00FF08;width: 0px;height: 98%;position: absolute;top: 0px;left: '+(relX-42)+'px;"></span>');
 
-			$current_line = {lineReference: lineReference,valueHour:valueHour};
+			var valueHour = $('<span id="valueHour" style="top: '+(relY)+'px;z-index:9999;background-color: whitesmoke;color:red;width: 60px;height: 20px;position: absolute;left: -70px;padding: 4px;border-radius: 5px;text-align: center;border: 1px solid silver;"><b>'+(result)+'h</b></span>');
+
+			$current_line = {lineReference: lineHReference,valueHour:valueHour};
 		   
-		    $("#ghx-chart-view").append(lineReference);
+		    $("#ghx-chart-view").append(lineHReference);
+		    $("#ghx-chart-view").append(lineVReference);
 			$("#ghx-chart-view").append(valueHour);
 
 		    $("#ghx-chart-view").append($line_cache['lineReference']);
